827b6332f100437619b2c55afc526d5e06f6873a
AYKupenov21 <AYKupenov21@codingburgas.bg>
2024-04-22 12:50:16 +0300

    Modify code

--- Task4/Task4/Task4.cpp
+++ Task4/Task4/Task4.cpp
@@ -27,23 +27,38 @@ public:
     {
         return cordinate3;
     }
-    void setCordinate1(int newCordinate1) { cordinate1 = newCordinate1; }
-    void setCordinate2(int newCordinate2) { cordinate2 = newCordinate2; }
-    void setCordinate3(int newCordinate3) { cordinate3 = newCordinate3; }
+    void setCordinate1(int newCordinate1)
+    {
+        cordinate1 = newCordinate1; 
+    }
+    void setCordinate2(int newCordinate2)
+    {
+        cordinate2 = newCordinate2;
+    }
+    void setCordinate3(int newCordinate3) 
+    {
+        cordinate3 = newCordinate3;
+    }
 };
 
 int main() {
     Point point1(1, 2, 3);
     Point point2(4, 5, 6);
+    int point;
+    int newCordinate1, newCordinate2, newCordinate3;
     cout << "Point 1 coordinates: (" << point1.getCordinate1() << ", " << point1.getCordinate2() << ", " << point1.getCordinate3() << ")" << endl;
     cout << "Point 2 coordinates: (" << point2.getCordinate1() << ", " << point2.getCordinate2() << ", " << point2.getCordinate3() << ")" << endl;
-    point1.setCordinate1(10);
-    point1.setCordinate2(20);
-    point1.setCordinate3(30);
-    cout << "New coordinates of Point 1: (" << point1.getCordinate1() << ", " << point1.getCordinate2() << ", " << point1.getCordinate3() << ")" << endl;
+    cout << "Which point do you want to change?  ";
+    cin >> point;
+    cout << "Enter new cordinates: ";
+    cin >> newCordinate1 >> newCordinate2 >> newCordinate3;
+    point1.setCordinate1(newCordinate1);
+    point1.setCordinate2(newCordinate2);
+    point1.setCordinate3(newCordinate3);
+    cout << "New coordinates of Point "<< point <<": (" << point1.getCordinate1() << ", " << point1.getCordinate2() << ", " << point1.getCordinate3() << ")" << endl;
     Point point1Modify(point1);
-    point1Modify.setCordinate1(point1Modify.getCordinate1() + 1);
-    point1Modify.setCordinate2(point1Modify.getCordinate2() + 2);
-    point1Modify.setCordinate3(point1Modify.getCordinate3() + 3);
-    cout << "Coordinates of Point 1 (modified constructor): (" << point1Modify.getCordinate1() << ", " << point1Modify.getCordinate2() << ", " << point1Modify.getCordinate3() << ")" << endl;
+    point1Modify.setCordinate1(point1Modify.getCordinate1());
+    point1Modify.setCordinate2(point1Modify.getCordinate2());
+    point1Modify.setCordinate3(point1Modify.getCordinate3());
+    cout << "Coordinates of Point " << point <<" (modified constructor): (" << point1Modify.getCordinate1() << ", " << point1Modify.getCordinate2() << ", " << point1Modify.getCordinate3() << ")" << endl;
 }
